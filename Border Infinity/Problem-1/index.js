5e783bb99eca766fc9eaa3478edb0e7581e30621
ranjit-sudo <55087468+ranjit-sudo@users.noreply.github.com>
2021-07-03 19:07:18 +0530

    Delete Border Infinity directory

--- Border Infinity/Problem-1/index.js
+++
@@ -1,8 +0,0 @@
-const background = () => {
-  const randCol = Math.floor(Math.random() * 16777215).toString(16);
-  document.body.style.backgroundColor = "#" + randCol;
-  hex_code.innerHTML = "HEX-Color: " + "#" + randCol;
-};
-
-change_color.addEventListener("click", background);
-background();
